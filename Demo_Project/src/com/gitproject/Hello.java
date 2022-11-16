df63cbf8fca872f0a4b4abecc222b00d2836c5ef
unknown <sumitmali3567@gmail.com>
2022-11-16 16:48:41 +0530

    first commit

---
+++ Demo_Project/src/com/gitproject/Hello.java
@@ -0,0 +1,10 @@
+package com.gitproject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO GITHUB !!!");
+
+	}
+
+}
---
+++ Demo_Project/src/com/gitproject/Hello1.java
@@ -0,0 +1,9 @@
+package com.gitproject;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO EVERYONE GIT HUB IS OPEN FOR ALL NOW !!!");
+	}
+
+}
